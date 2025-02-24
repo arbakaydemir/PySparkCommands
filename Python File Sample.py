c4e5d9f7ebaa81a41fd27951b2fde82ea9cc8d72
Arbak Aydemir <arbak.aydemir@sita.aero>
2025-02-24 16:01:37 +0000

    Adding Comment

--- Python File Sample.py
+++ Python File Sample.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     name = input("Enter your name: ")
     print(greet(name))
 
-print("Thank you for using this program!")
+print("Thank you for using this program!")  # This line is not part of the function
+# End of Python File Sample.py
