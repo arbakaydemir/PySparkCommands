30e91c72c8db3cc6be3e3456d2aeeb2939a07b6c
Arbak Aydemir <arbak.aydemir@sita.aero>
2025-02-25 15:19:07 +0000

    New

--- Python File Sample.py
+++ Python File Sample.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
     name = input("Enter your name: ")
     print(greet(name))
 
-<<<<<<< HEAD
 print("Thank you for using this program!")  # This line is not part of the function
 # End of Python File Sample.py
-=======
-print("Thank you for using this program!")  # This line is not part of the function
->>>>>>> 6a8156679d4220f8b4a5acbcfc52f1e1f7c776a6
+
